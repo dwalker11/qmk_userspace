ceaa2eda5e281d2322133c7cb140d74e04bb6d74
Devon Walker <devonwalker@Mac.localdomain>
2025-09-29 01:42:21 -0400

    Define keyboard layer alias

--- keyboards/preonic/rev3_drop/keymaps/dwalker11/keymap.c
+++ keyboards/preonic/rev3_drop/keymaps/dwalker11/keymap.c
@@ -47,7 +47,14 @@ enum preonic_keycodes {
   BACKLIT
 };
 
-// Right-hand home row mods
+// Escape Control
+#define CTL_ESC CTL_T(KC_ESC)
+
+// Left-hand layer-tap mods
+#define NAV_A LT(NAV, KC_A)
+#define SYMB_F LT(RAISE, KC_F)
+
+// Right-hand home-row mods
 #define GUI_J RGUI_T(KC_J)
 #define SFT_K RSFT_T(KC_K)
 #define ALT_L LALT_T(KC_L)
@@ -90,11 +97,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_GRV,           KC_1,        KC_2,    KC_3,    KC_4,         KC_5,   KC_6,   KC_7,       KC_8,    KC_9,    KC_0,     KC_BSPC,        \
-  KC_TAB,           KC_Q,        KC_W,    KC_E,    KC_R,         KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,     KC_DEL,         \
-  CTL_T(KC_ESC),  LT(NAV, KC_A), KC_S,    KC_D, LT(RAISE, KC_F), KC_G,   KC_H,   GUI_J,      SFT_K,   ALT_L,   CTL_SCLN, KC_QUOT,        \
-  KC_LSFT,          KC_Z,        KC_X,    KC_C,    KC_V,         KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT), \
-  OSL(6),           OS_LCTL,     OS_LGUI, OS_LALT, MO(LOWER),    KC_SPC, KC_SPC, OSL(RAISE), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT         \
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,   KC_6,   KC_7,       KC_8,    KC_9,    KC_0,     KC_BSPC,        \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,     KC_DEL,         \
+  CTL_ESC, NAV_A,   KC_S,    KC_D,    SYMB_F,    KC_G,   KC_H,   GUI_J,      SFT_K,   ALT_L,   CTL_SCLN, KC_QUOT,        \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT), \
+  OSL(6),  OS_LCTL, OS_LGUI, OS_LALT, MO(LOWER), KC_SPC, KC_SPC, OSL(RAISE), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT         \
 ),
 
 /* Colemak
