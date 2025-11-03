855dd32eb5696cd2eb063f272a8c31fddddcec58
Devon <devon.dana@gmail.com>
2025-11-02 19:16:24 -0500

    Update num and game layers

--- keyboards/preonic/rev3_drop/keymaps/dwalker11/keymap.c
+++ keyboards/preonic/rev3_drop/keymaps/dwalker11/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,     KC_DEL,         \
   CTL_ESC, NAV_A,   KC_S,    KC_D,    SYMB_F,    KC_G,   KC_H,   GUI_J,      SFT_K,   ALT_L,   CTL_SCLN, KC_QUOT,        \
   OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  RSFT_T(KC_ENT), \
-  OSL(6),  OS_LCTL, OS_LGUI, OS_LALT, TG(LOWER), KC_SPC, KC_SPC, OSL(RAISE), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT         \
+  OSL(6),  OS_LCTL, OS_LGUI, OS_LALT, MO(LOWER), KC_SPC, KC_SPC, OSL(RAISE), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT         \
 ),
 
 /* Colemak
@@ -160,10 +160,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_grid( \
-  _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_DOT,  _______, \
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX, _______, \
-  _______, KC_F9,   KC_F11,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, _______, \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    QK_LLCK, _______, \
+  _______, KC_F9,   KC_F11,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, KC_0,    KC_DOT,  XXXXXXX, _______, \
   _______, _______, _______, _______, _______, _______, _______, MO(6),   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT  \
 ),
 
